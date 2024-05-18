3276e1c3af5f49b8536db15ea6e406aad6549eef
MasterHell <123289351+MasterMaybeFromHell@users.noreply.github.com>
2024-05-18 14:04:55 +0300

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using AdvancedLogSystem;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(MainScript), "AdvancedLogSystem", "1.4", "MasterHell")]
+[assembly: MelonColor(System.ConsoleColor.DarkYellow)]
