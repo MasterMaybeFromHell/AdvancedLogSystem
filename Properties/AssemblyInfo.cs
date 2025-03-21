d43a699daf1ee2710c765f05d8adfbfdcaac39a9
MasterHell <123289351+MasterMaybeFromHell@users.noreply.github.com>
2025-03-21 10:40:40 +0200

    Delete Properties directory

--- Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using AdvancedLogSystem;
-using MelonLoader;
-
-[assembly: MelonInfo(typeof(MainScript), "AdvancedLogSystem", "1.4", "MasterHell")]
-[assembly: MelonColor(System.ConsoleColor.DarkYellow)]
